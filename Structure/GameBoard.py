564109b79c713ad732c023a1a94f59ddeeb79c63
Random-Cookie <sefi100@ntlworld.com>
2021-01-23 19:26:55 +0000

    -GameBoard can now construct a graph to represent the board -A sample config file is given in classic.txt to represent the original board game(sample of cities and double tracks -Single pass over the nodes can creates entire board -Replaced __contains__ with in

--- Structure/GameBoard.py
+++ Structure/GameBoard.py
@@ -4,15 +4,35 @@ from Structure.Nodes import *
 
 class GameBoard:
 	def __add_node(self, cities: dict, node_id: str):
-		if cities.keys().__contains__(node_id):
-			city = City(node_id, cities.get(node_id))
-			self.cities.append(city)
-			self.map.add_node(city)
+		"""Add a node to the map, deciding whether to add a city or normal node should be added
+		:param cities: Cities Dictionary
+		:param node_id: ID of node to add
+		:return: None
+		"""
+		node = None
+		if node_id in cities.keys():
+			node = City(node_id, cities.get(node_id))
+			self.cities[node_id] = node
+			self.map.add_node(node)
 		else:
-			self.map.add_node(Node(node_id))
+			node = Node(node_id)
+			self.map.add_node(node)
+		self.nodes[node_id] = node
+
+	def __add_edge(self, double_tracks: dict, start: Node, end: Node):
+		if start.get_id() in double_tracks.keys() and end.get_id() in double_tracks.get(start.get_id()):
+			edge = (start, end, {'weight': 2})
+			self.map.add_edges_from([edge])
+		else:
+			edge = (start, end, {'weight': 1})
+			self.map.add_edges_from([edge])
 
 	@staticmethod
-	def map_cities(city_list) -> dict:
+	def map_cities(city_list: str) -> dict:
+		""" turn cities list into a dictionary
+		:param city_list: List of cities in form of a string from config file
+		:return: a dictionary of all cities where: node_id -> name
+		"""
 		city_dict = {}
 		city_list = city_list.splitlines()
 		for city in city_list:
@@ -20,7 +40,22 @@ class GameBoard:
 			city_dict[city_data[0]] = city_data[1]
 		return city_dict
 
+	@staticmethod
+	def map_double_tracks(track_list: []) -> dict:
+		track_dict = {}
+		for track in track_list:
+			track = track.split(',')
+			if track_dict.keys().__contains__(track[0]):
+				track_dict[track[0]].append(track[1])
+			else:
+				track_dict[track[0]] = [track[1]]
+		return track_dict
+
 	def __generate_map(self, filepath: str):
+		"""Generate a game map from a config file
+		:param filepath: File path of the map config file
+		:return: none
+		"""
 		ret = nx.Graph()
 		# Read map file
 		map_file = open(filepath)
@@ -29,20 +64,32 @@ class GameBoard:
 		# ":\n" denotes end of config title
 		rows = map_data[1].split(':\n')[1].splitlines()
 		cities = self.map_cities(map_data[2].split(':\n')[1])
-
+		double_tracks = self.map_double_tracks(map_data[3].split(':\n')[1].splitlines())
+		# build map nodes and edges
 		for i in range(0, len(rows)):
-			# print(str(i) + ": " + rows[i])
 			# Add initial node for each row at starting position
 			row_data = rows[i].split(',')
 			start_val = int(row_data[0])
 			row_len = int(row_data[1])
-			self.__add_node(cities, str(start_val).rjust(2, '0') + str(i).rjust(2, '0'))
-			for j in range(1, row_len):
-				self.__add_node(cities, str(start_val + (2 * j)).rjust(2, '0') + str(i).rjust(2, '0'))
+			prev_node_id = "0000"
+			for j in range(0, row_len):
+				node_id = str(start_val + (2 * j)).rjust(2, '0') + str(i).rjust(2, '0')
+				self.__add_node(cities, node_id)
+				if j != 0:  # no horizontal edge needed for first node in row
+					self.__add_edge(double_tracks, self.nodes[node_id], self.nodes[prev_node_id])
+				if i != 0:  # no intermediate edges needed for first row
+					top_left = str((start_val - 1) + (2 * j)).rjust(2, '0') + str(i - 1).rjust(2, '0')
+					top_right = str((start_val + 1) + (2 * j)).rjust(2, '0') + str(i - 1).rjust(2, '0')
+					if top_left in self.nodes.keys():
+						self.__add_edge(double_tracks, self.nodes.get(node_id), self.nodes.get(top_left))
+					if top_right in self.nodes.keys():
+						self.__add_edge(double_tracks, self.nodes.get(node_id), self.nodes.get(top_right))
+				prev_node_id = node_id
 
-	def __init__(self, players, map_filepath: str):
+	def __init__(self, players: [], map_filepath: str):
 		self.players = players
-		self.cities = []
+		self.cities = {}
+		self.nodes = {}
 		self.map = nx.Graph()
 		self.__generate_map(map_filepath)
 
--- Structure/Nodes.py
+++ Structure/Nodes.py
@@ -1,9 +1,16 @@
 class Node:
 	def __init__(self, node_id: str):
-		self.id = node_id
+		self.__id = node_id
+
+	def get_id(self) -> str:
+		return self.__id
+
 
 
 class City(Node):
 	def __init__(self, node_id: str, name: str):
 		Node.__init__(self, node_id)
 		self.name = name
+
+	def get_name(self) -> str:
+		return self.name
